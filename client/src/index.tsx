f2f7e2554ce43a03ea75132131929e247eb37a53
unknown <axmedobsiye15@gmail.com>
2023-01-26 10:21:00 +0300

    adding typescript to our project for fast performance and less error code

---
+++ client/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.tsx';
+import registerServiceWorker from './registerServiceWorker.tsx';
+import 'tachyons';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
